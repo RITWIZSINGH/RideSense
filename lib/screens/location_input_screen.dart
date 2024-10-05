fabff77f1b45e8c355c326ee88f33e3b672a0c2a
Ritwiz <madangopalsingh2803@gmail.com>
2024-10-06 02:37:14 +0530

    Making the basic structure of the app

---
+++ lib/screens/location_input_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class LocationInputScreen extends StatefulWidget {
+  const LocationInputScreen({super.key});
+
+  @override
+  State<LocationInputScreen> createState() => _LocationInputScreenState();
+}
+
+class _LocationInputScreenState extends State<LocationInputScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
